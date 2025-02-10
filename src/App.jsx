24a4b6151a6fa275fbe027c974eb6bd42291d5e5
smanaman <sanvatsman@gmail.com>
2025-02-10 17:03:15 +0530

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,37 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  const add=()=>{
-    setCount(count+1)
+  const add = () => {
+    setCount(count + 1)
   }
-  const minus=()=>{
-if(count==0){
-  setCount(0)
-}else{
-  setCount(count-1)
-}
+  const minus = () => {
+    if (count == 0) {
+      setCount(0)
+    } else {
+      setCount(count - 1)
+    }
+  }
+  const reset = () => {
+    setCount(0)
   }
   return (
-  <>
-  <button onClick={()=>add()}>+</button>
-  <p>{count}</p>
-  <button onClick={()=>minus()}>-</button>
-  </>
+    <>
+
+
+      <div className="main-btn">
+        <div className="p">
+        <p>{count}</p>
+        </div>
+        <div className="btn-group-one">
+          <button onClick={() => minus()}><i class="fa-solid fa-minus"></i></button>
+       
+          <button onClick={() => add()}><i class="fa-solid fa-plus"></i></button>
+        </div>
+        <div className="reset-btn">
+          <button onClick={() => reset()}>reset</button>
+        </div>
+      </div>
+    </>
   )
 }
 
